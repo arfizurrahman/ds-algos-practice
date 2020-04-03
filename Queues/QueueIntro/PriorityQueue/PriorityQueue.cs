6d046df30ccbf2f8ed53ede94682249010c88feb
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-04-04 00:51:35 +0600

    Implementing a priority queue from scratch

---
+++ Queues/QueueIntro/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    class PriorityQueue
+    {
+        private int[] items;
+        private int length;
+
+        public PriorityQueue(int length)
+        {
+            items = new int[length];
+        }
+
+        public void Add(int item)
+        {
+            if (IsFull())
+                throw new InvalidOperationException();
+
+            var i = ShiftItemsToInsert(item);
+            items[i + 1] = item;
+            length++;
+        }
+
+        private int ShiftItemsToInsert(int item)
+        {
+            int i;
+            for (i = length - 1; i >= 0; i--)
+            {
+                if (item < items[i])
+                    items[i + 1] = items[i];
+                else
+                    break;
+            }
+
+            return i + 1;
+        }
+
+        public int Remove()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException();
+
+            return items[--length];
+        }
+
+        public bool IsEmpty()
+        {
+            return length == 0;
+        }
+
+        public bool IsFull()
+        {
+            return length == items.Length;
+        }
+
+        override
+        public string ToString()
+        {
+            return string.Join(", ", items);
+        }
+    }
+}
---
+++ Queues/QueueIntro/PriorityQueue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue queue = new PriorityQueue(10);
+            queue.Add(2);
+            queue.Add(3);
+            queue.Add(1);
+            queue.Add(6);
+
+            //Console.WriteLine(queue);
+            //Console.WriteLine("Dequeue: " + queue.Remove());
+            //Console.WriteLine(queue);
+            //Console.WriteLine("Dequeue: " + queue.Remove());
+            //Console.WriteLine("Dequeue: " + queue.Remove());
+            while (!queue.IsEmpty())
+            {
+                Console.WriteLine(queue.Remove());
+            }
+            Console.ReadKey();
+        }
+    }
+}
