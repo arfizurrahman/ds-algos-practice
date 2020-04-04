82c5b73c0210e92c86e9b70d1ec0f87dc592da4b
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-04-04 20:34:50 +0600

    Created stack using two queues

---
+++ Stacks/StacksIIntro/StackUsingTwoQueues/StackWithTwoQueues.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingTwoQueues
+{
+    class StackWithTwoQueues
+    {
+        private Queue<int> queue1 = new Queue<int>();
+        private Queue<int> queue2 = new Queue<int>();
+        private int top;
+
+        public void Push(int value)
+        {
+            queue1.Enqueue(value);
+            top = value;
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException();
+
+            while (queue1.Count > 1)
+            {
+                top = queue1.Dequeue();
+                queue2.Enqueue(top);
+            }
+            
+            SwapQueues();
+            
+            return queue2.Dequeue();
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException();
+
+            return top;
+        }
+
+        public int Count()
+        {
+            return queue1.Count;
+        }
+
+        private void SwapQueues()
+        {
+            var temp = queue1;
+            queue1 = queue2;
+            queue2 = temp;
+        }
+
+        public bool IsEmpty()
+        {
+            return queue1.Count == 0;
+        }
+    }
+}
