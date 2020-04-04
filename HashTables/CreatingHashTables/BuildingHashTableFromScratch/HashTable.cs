8227ab7654b4f1c5f4cdb2162ffafd425fd0435e
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-04-05 01:34:48 +0600

    Completed building hashtable

---
+++ HashTables/CreatingHashTables/BuildingHashTableFromScratch/HashTable.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingHashTableFromScratch
+{
+    class HashTable
+    {
+        private class Entry
+        {
+            public int key;
+            public string value;
+
+            public Entry(int key, string value)
+            {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        private LinkedList<Entry>[] items = new LinkedList<Entry>[5];
+
+        public void Add(int key, string value)
+        {
+            var entry = GetEntry(key);
+            if (entry != null) {
+                entry.value = value;
+                return;
+            }
+            
+            GetOrCreateBucket(key).AddLast(new Entry(key, value));
+        }
+
+        public string GetValue(int key)
+        {
+            var entry = GetEntry(key);
+            return (entry == null) ? null : entry.value;
+        }
+
+        public void Remove(int key)
+        {
+            var entry = GetEntry(key);
+            if (entry == null)
+                throw new InvalidOperationException();
+
+            GetBucket(key).Remove(entry);
+        }
+
+        private Entry GetEntry(int key)
+        {
+            var bucket = GetBucket(key);
+            if (bucket != null)
+            {
+                foreach (var item in bucket)
+                {
+                    if (item.key == key)
+                        return item;
+                }
+            }
+
+            return null;
+        }
+
+        private LinkedList<Entry> GetBucket(int key)
+        {
+            return items[Hash(key)];
+        }
+
+        private LinkedList<Entry> GetOrCreateBucket(int key)
+        {
+            var index = Hash(key);
+            var bucket = items[index];
+            if (bucket == null)
+            {
+                items[index] = new LinkedList<Entry>();
+                bucket = items[index];
+            }
+
+            return bucket;
+        }
+
+        private int Hash(int key)
+        {
+            return key % items.Length;
+        }
+    }
+}
---
+++ HashTables/CreatingHashTables/BuildingHashTableFromScratch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingHashTableFromScratch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable hashTable = new HashTable();
+            hashTable.Add(6, "Arfiz");
+            hashTable.Add(8, "Fahmida");
+            hashTable.Add(11, "Ashfaq");
+            hashTable.Add(11, "Farez");
+            hashTable.Remove(9);
+
+            Console.WriteLine(hashTable.GetValue(3));
+            Console.ReadKey();
+        }
+    }
+}
