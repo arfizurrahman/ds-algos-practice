4fd0a52c03153c2e1bc8e6c3e526cb6a700b77dd
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-04-14 02:41:47 +0600

    Implemented prim's algorithm

--- Graphs/GraphsJava/src/com/arfiz/weightedgraph/Main.java
+++ Graphs/GraphsJava/src/com/arfiz/weightedgraph/Main.java
@@ -6,12 +6,14 @@ public class Main {
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
-        graph.addEdge("A", "B", 1);
+        graph.addNode("D");
+        graph.addEdge("A", "B", 3);
+        graph.addEdge("B", "D", 4);
+        graph.addEdge("C", "D", 5);
+        graph.addEdge("A", "C", 1);
         graph.addEdge("B", "C", 2);
-        graph.addEdge("C", "A", 10);
         //var path = graph.getShortestPath("A", "C");
-
-
-        System.out.println(graph.hasCycle());
+        var tree= graph.getMinimumSpanningTree();
+        tree.print();
     }
 }
