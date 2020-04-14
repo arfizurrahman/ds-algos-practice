95ad58b66b897339a6499205ad809ee36c677f60
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-04-14 11:58:00 +0600

    Implemented selection sort

---
+++ SortingAlgorithms/SortingAlgorithms/SelectionSort/SelectionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class SelectionSort
+    {
+        public void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int minIndex = i;
+                for (int j = i; j < array.Length; j++)
+                    if (array[minIndex] > array[j])
+                        minIndex = j;
+                Swap(array, minIndex, i);
+            }
+        }
+
+        private void Swap(int[] array, int index1, int index2)
+        {
+            var temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+        }
+    }
+}
