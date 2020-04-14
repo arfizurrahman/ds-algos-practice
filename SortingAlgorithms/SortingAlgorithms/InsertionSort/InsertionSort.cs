f1a8abdfdd9c78177060b2891250a04d94d2b595
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-04-14 16:48:22 +0600

    Implemented insertion sort

---
+++ SortingAlgorithms/SortingAlgorithms/InsertionSort/InsertionSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class InsertionSort
+    {
+        public void Sort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                var current = array[i];
+                var j = i - 1;
+                while (j >= 0 && array[j] > current)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = current;
+            }
+        }
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithms/InsertionSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { };
+            var sorter = new InsertionSort();
+            sorter.Sort(numbers);
+
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.ReadKey();
+        }
+    }
+}
