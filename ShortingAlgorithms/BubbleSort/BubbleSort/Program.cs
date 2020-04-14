e14d55685f2b790c3421786bca1dda7d7821aa83
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-04-14 10:53:07 +0600

    Implemented bubblesort

---
+++ ShortingAlgorithms/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {8, 2, 4, 1, 3};
+            var sorter = new BubbleSort();
+            sorter.Sort(numbers);
+
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.ReadKey();
+        }
+        
+    }
+}
