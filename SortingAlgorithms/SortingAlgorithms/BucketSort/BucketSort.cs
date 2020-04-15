a7868b9b741812c0547e0367f84699577407ca09
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-04-15 22:15:06 +0600

    Implementent bucket sort

---
+++ SortingAlgorithms/SortingAlgorithms/BucketSort/BucketSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BucketSort
+{
+    class BucketSort
+    {
+        public void Sort(int[] array, int numberOfBuckets)
+        {
+            var i = 0;
+            foreach (var bucket in CreateBuckets(array, numberOfBuckets))
+            {
+                bucket.Sort();
+                foreach (var item in bucket)
+                    array[i++] = item;
+            }
+
+        }
+
+        private List<List<int>> CreateBuckets(int[] array, int numberOfBuckets)
+        {
+            List<List<int>> buckets = new List<List<int>>();
+
+            for (int i = 0; i < numberOfBuckets; i++)
+                buckets.Add(new List<int>());
+
+            foreach (var item in array)
+                buckets[item / numberOfBuckets].Add(item);
+
+            return buckets;
+        }
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithms/BucketSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BucketSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 8, 2, 4, 1, 3, 8 };
+            var sorter = new BucketSort();
+            sorter.Sort(numbers, 3);
+
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.ReadKey();
+        }
+    }
+}
